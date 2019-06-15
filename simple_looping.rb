003ee10162c66698464f310488fb5bc25293a7a5
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-15 19:46:59 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -42,7 +42,7 @@ end
 end
 
 def for_iterator(number_of_times)
-  counter = 0..number_of_times
+  counter = 1..number_of_times
   for counter in 0..number_of_times do
     puts "Welcome to Flatiron School's Web Development Course!"
     counter - 1
