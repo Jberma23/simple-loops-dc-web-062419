c886fc794302a194b22d01241786e7f6bbdc34b0
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-15 19:37:44 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,7 +21,7 @@ end
 
 def times_iterator(number_of_times)
   number_of_times.times do
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  puts "Welcome to Flatiron School's Web Development Course!"
 end
 end
 
